01f0a400422b01af6693eae42e2db79e431856a3
manfer993 <manfer93@gmail.com>
2020-08-05 11:41:12 -0500

    put final constans

--- src/utils/Locators.java
+++ src/utils/Locators.java
@@ -1,36 +1,36 @@
 package utils;
 
-public class Locators {
+public final class Locators {
 	// LogInPage
-	public static String username_textbox_id = "user-name";
-	public static String password_textbox_id = "password";
-	public static String login_button_id = "login-button";
-	public static String errormessage_h3_xpath = "//h3[@data-test='error']";
+	public static final String username_textbox_id = "user-name";
+	public static final String password_textbox_id = "password";
+	public static final String login_button_id = "login-button";
+	public static final String errormessage_h3_xpath = "//h3[@data-test='error']";
 	
 	// HomePage
-	public static String menuclose_button_xpath = "//button[text()='Close Menu']";
-	public static String menuopen_button_xpath = "//button[text()='Open Menu']";
-	public static String logout_button_id = "logout_sidebar_link";
-	public static String productitem_div_class = "inventory_item";
-	public static String productname_div_class = "inventory_item_name";
-	public static String addproduct_button_class = "btn_primary";
-	public static String removeproduct_button_class = "btn_secondary";
-	public static String shopping_button_id = "shopping_cart_container";
+	public static final String menuclose_button_xpath = "//button[text()='Close Menu']";
+	public static final String menuopen_button_xpath = "//button[text()='Open Menu']";
+	public static final String logout_button_id = "logout_sidebar_link";
+	public static final String productitem_div_class = "inventory_item";
+	public static final String productname_div_class = "inventory_item_name";
+	public static final String addproduct_button_class = "btn_primary";
+	public static final String removeproduct_button_class = "btn_secondary";
+	public static final String shopping_button_id = "shopping_cart_container";
 	
 	// CartPage
-	public static String pagetitle_div_class = "subheader";
-	public static String checkout_button_xpath = "//a[@href='./checkout-step-one.html']";
+	public static final String pagetitle_div_class = "subheader";
+	public static final String checkout_button_xpath = "//a[@href='./checkout-step-one.html']";
 	
 	// CheckoutStepOnePage	
-	public static String firstname_textbox_id = "first-name";
-	public static String lastname_textbox_id = "last-name";
-	public static String postalcode_textbox_id = "postal-code";
-	public static String continue_Button_xpath = "//*[@value='CONTINUE']";
+	public static final String firstname_textbox_id = "first-name";
+	public static final String lastname_textbox_id = "last-name";
+	public static final String postalcode_textbox_id = "postal-code";
+	public static final String continue_Button_xpath = "//*[@value='CONTINUE']";
 	
 	// CheckoutStepTwoPage
-	public static String finish_button_xpath = "//a[@href='./checkout-complete.html']";
+	public static final String finish_button_xpath = "//a[@href='./checkout-complete.html']";
 	
 	// CheckoutCompletePage
-	public static String shoppingresponse_text_class = "complete-header";
+	public static final String shoppingresponse_text_class = "complete-header";
 
 }
--- src/utils/Messages.java
+++ src/utils/Messages.java
@@ -1,23 +1,23 @@
 package utils;
 
-public class Messages {
+public final class Messages {
 	// LogInPage
-	public static String errorMessage_locked_out_user = "Epic sadface: Sorry, this user has been locked out.";
+	public static final String errorMessage_locked_out_user = "Epic sadface: Sorry, this user has been locked out.";
 	
 	// CartPage
-	public static String subHeader_cart = "Your Cart";
+	public static final String subHeader_cart = "Your Cart";
 	
 	// CheckoutStepOnePage
-	public static String subHeader_checkout_step_one = "Checkout: Your Information";
-	public static String errormessage_first_name_missing = "Error: First Name is required";
-	public static String errormessage_last_name_missing = "Error: Last Name is required";
-	public static String errormessage_postal_code_missing = "Error: Postal Code is required";
+	public static final String subHeader_checkout_step_one = "Checkout: Your Information";
+	public static final String errormessage_first_name_missing = "Error: First Name is required";
+	public static final String errormessage_last_name_missing = "Error: Last Name is required";
+	public static final String errormessage_postal_code_missing = "Error: Postal Code is required";
 	
 	// CheckoutStepTwoPage
-	public static String subHeader_checkout_step_two = "Checkout: Overview";
+	public static final String subHeader_checkout_step_two = "Checkout: Overview";
 	
 	// CheckoutCompletePage
-	public static String subHeader_checkout_complete = "Finish";
-	public static String shopping_response = "THANK YOU FOR YOUR ORDER";
+	public static final String subHeader_checkout_complete = "Finish";
+	public static final String shopping_response = "THANK YOU FOR YOUR ORDER";
 
 }
